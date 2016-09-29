caf2b0f35dccd711d6abdfaaf642372355b4f3c2
Max Serebryanskiy <maxim.serebryanskiy@icloud.com>
2016-09-29 16:46:39 +0300

    add example test for after_failure

---
+++ src/test/groovy/finance/ForAfterFailureSpec.groovy
@@ -0,0 +1,25 @@
+package finance
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+import java.time.Period
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(AccountService)
+@Mock([Account, Balance, Operation])
+class ForAfterFailureSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "broken test for after_failure"() {
+        throw new RuntimeException('For after_failure')
+    }
+}
